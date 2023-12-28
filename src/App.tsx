8d07df1b366ddd3cfd0600f25d776e898a21b636
S.Berke.B <95080598+SBerkeB@users.noreply.github.com>
2023-12-28 19:49:02 +0300

    Update App.tsx
    Decreased the image generator's version.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     setProgress(10);
     setProgressLabel('Starting');
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/dataautogpt3/OpenDalleV1.1",
+      "https://api-inference.huggingface.co/models/dataautogpt3/OpenDalle",
       {
         headers: { Authorization: "Bearer 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" },
         method: "POST",
