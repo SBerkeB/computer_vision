680879d2bd3d9f335c18d1ef777f55180cf81012
S.Berke.B <95080598+SBerkeB@users.noreply.github.com>
2024-02-20 16:21:01 +0300

    Update App.tsx
    Deleted auth token

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
     const response = await fetch(
       "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
       {
-        headers: { Authorization: "Bearer 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" },
+        headers: { Authorization: "" },
         method: "POST",
         body: JSON.stringify({"inputs": ocrResult}),
       }
